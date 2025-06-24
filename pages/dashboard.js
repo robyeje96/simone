afc4e22bdf2cd16313a6d820317fa347bcf79467
robyeje96 <roberto.ciro@hotmail.it>
2025-06-24 13:43:48 +0200

    Create pages/dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div className="p-8 bg-gray-900 text-white">
+      <h2 className="text-3xl mb-2">Dashboard Segnali</h2>
+      <p>I segnali saranno visibili qui, aggiornati ogni 5 minuti.</p>
+    </div>
+  );
+}
